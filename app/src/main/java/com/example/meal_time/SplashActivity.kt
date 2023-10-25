d85cc251930e1aa9136b8047c73829890121c18e
shin-hyun-soo <shs6228@naver.com>
2023-10-25 22:15:19 +0900

    인트로 액티비티 이미지 추가 및 화면 전환

--- app/src/main/java/com/example/meal_time/SplashActivity.kt
+++ app/src/main/java/com/example/meal_time/SplashActivity.kt
@@ -1,11 +1,22 @@
 package com.example.meal_time
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.meal_time.auth.IntroActivity
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //handler 사용하여 delay를 3초 주고 IntroActivity 실행하고 스플래시화면 종료
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
