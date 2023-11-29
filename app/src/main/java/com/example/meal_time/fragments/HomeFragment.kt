84c1f737f57aa4c3e2869799834beb02cda5351f
SeolHaling <ok3096213@gmail.com>
2023-11-29 23:35:51 +0900

    main fragment에 할일 리스트 추가 타이머 추가

--- app/src/main/java/com/example/meal_time/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/meal_time/fragments/HomeFragment.kt
@@ -1,33 +1,31 @@
 package com.example.meal_time.fragments
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.meal_time.R
 import com.example.meal_time.databinding.FragmentHomeBinding
+import com.example.meal_time.todolist.Todo
+import com.example.meal_time.todolist.TodoAdapter
+import kotlinx.android.synthetic.main.fragment_home.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
 
     private lateinit var binding : FragmentHomeBinding
 
+    private lateinit var todoAdapter : TodoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -37,6 +35,31 @@ class HomeFragment : Fragment() {
     ): View? {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+
+        // Null check for RecyclerView
+        val rvTodoItems: RecyclerView = binding.rvTodoItems
+        rvTodoItems.layoutManager = LinearLayoutManager(context)
+        todoAdapter = TodoAdapter(mutableListOf())
+        rvTodoItems.adapter = todoAdapter
+        // Null check for EditText
+        //val etTodoTitle1: EditText = binding.etTodoTitle1
+
+
+        binding.btnAddTodo2.setOnClickListener {
+            val todoTitle = etTodoTitle.text?.toString() ?: ""
+            if (todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+            }
+        }
+
+        binding.btnDeleteDoneTodo2.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
+
+
         //서진
         binding.talkTap.setOnClickListener {
 
